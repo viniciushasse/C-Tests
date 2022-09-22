9b664b898e7d4d9a6b75a4ff707fec28a32bb183
viniciushasse <99216933+viniciushasse@users.noreply.github.com>
2022-09-22 10:19:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Operator
+{
+    class ArithmeticOperator
+    {
+        public static void Main(string[] args)
+        {
+            double x = 15, y = 5, result;
+            
+
+            // Addition operator
+            result = x + y;
+            Console.WriteLine("{0} + {1} = {2}", x, y, result);
+
+
+            // Subtraction operator
+            result = x - y;
+            Console.WriteLine("{0} - {1} = {2}", x, y, result);
+
+
+            // Multiplication operator
+            result = x * y;
+            Console.WriteLine("{0} * {1} = {2}", x, y, result);
+
+
+            // Division operator
+            result = x / y;
+            Console.WriteLine("{0} / {1} = {2}", x, y, result);
+
+       
+        }
+    }
+}
